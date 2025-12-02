6488b333571423d7805317b4555a28b140028698
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-12-02 20:20:17 +0600

    initial setup done : likes tailwindcss, daisyiu, google font,rootlayout,routing

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       
+       <div>
+        hey there
+       </div>
+       
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/router/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../LayOuts/RootLayout";
+import Home from "../pages/Home/Home";
+ 
+ 
+ 
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+        {
+            index:true,
+            Component: Home
+        }
+    ]
+    
+  },
+]);
